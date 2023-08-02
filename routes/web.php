b1f3dee9791a26bed7f94d973ef1d3ad4cbd64d2
kuswandi-ti <kuswandi.ti@gmail.com>
2023-08-02 15:32:45 +0700

    4th Commit : Admin Setup - Adding Multi Auth System with Breeze (Part 1)

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
-    return view('backend.dashboard.index');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
