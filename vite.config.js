f35278145b97c91d70c135aa9202e84c62903d17
kuswandi-ti <kuswandi.ti@gmail.com>
2023-08-03 11:27:15 +0700

    8th Commit : Admin Setup - Adding localization syntax in all static text

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    // build: {
+    //     outDir: './build/'
+    // },
     plugins: [
         laravel({
             input: [
@@ -9,6 +12,7 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
+            // buildDirectory: './build/',
         }),
     ],
 });
